916c8395b788bbba800fab1094e331aa6ef860b0
00JAS00 <julian.m.sosa.muchut@gmail.com>
2023-01-30 23:54:41 -0300

    .

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
           <div class="top-row">
             <div class="field-wrap">
               <label>
-                Primer <span class="req">*</span>
+                Primer nombre <span class="req">*</span>
               </label>
               <input type="text" name="nombre"id="nombre" required autocomplete="off"/>
             </div>
